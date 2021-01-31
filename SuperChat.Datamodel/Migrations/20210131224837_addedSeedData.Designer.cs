edb4d423ffe644ebe43b3c399a8dea618850125d
Edwardenis de los Santos <edwardenis.santos@solvex.com.do>
2021-01-31 19:07:39 -0400

    Added Seed Method

--- SuperChat.Datamodel/Migrations/20210131224837_addedSeedData.Designer.cs
+++ SuperChat.Datamodel/Migrations/20210131224837_addedSeedData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperChat.Datamodel.Contexts;
 
 namespace SuperChat.Datamodel.Migrations
 {
     [DbContext(typeof(SuperChatDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210131224837_addedSeedData")]
+    partial class addedSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SuperChat.Datamodel/Migrations/20210131224837_addedSeedData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperChat.Datamodel.Migrations
+{
+    public partial class addedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ChatRooms",
+                columns: new[] { "Id", "Code", "CreatedAt", "CreatedBy", "Deleted", "Name", "UpdatedAt", "UpdatedBy" },
+                values: new object[] { 1, "Test1", new DateTimeOffset(new DateTime(2021, 1, 31, 22, 48, 37, 548, DateTimeKind.Unspecified).AddTicks(6155), new TimeSpan(0, 0, 0, 0, 0)), null, false, "Default Chat room", null, null });
+
+            migrationBuilder.InsertData(
+                table: "ChatRooms",
+                columns: new[] { "Id", "Code", "CreatedAt", "CreatedBy", "Deleted", "Name", "UpdatedAt", "UpdatedBy" },
+                values: new object[] { 2, "Test2", new DateTimeOffset(new DateTime(2021, 1, 31, 22, 48, 37, 548, DateTimeKind.Unspecified).AddTicks(6907), new TimeSpan(0, 0, 0, 0, 0)), null, false, "Default Chat room 2", null, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ChatRooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ChatRooms",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
