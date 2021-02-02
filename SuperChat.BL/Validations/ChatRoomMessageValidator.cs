4a1763404e7b3fcc724d87dc93eacb279812d6e4
Edwardenis de los Santos <edwardenis.santos@solvex.com.do>
2021-02-01 20:54:31 -0400

    Added validations for ChatRoomMessagesValidator

--- SuperChat.BL/Validations/ChatRoomMessageValidator.cs
+++ SuperChat.BL/Validations/ChatRoomMessageValidator.cs
@@ -1,16 +1,23 @@
 ﻿using FluentValidation;
 using SuperChat.BL.DTOs;
 using SuperChat.BL.Validations.Base;
+using SuperChat.Datamodel.Contexts;
+using SuperChat.Datamodel.Entities;
+using SuperChat.Datamodel.UnitOfWork;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SuperChat.BL.Validations
 {
     public class ChatRoomMessageValidator : BaseValidator<ChatRoomMessageDto>
     {
-        public ChatRoomMessageValidator()
+        private readonly IUnitOfWork<SuperChatDbContext> _uow;
+        public ChatRoomMessageValidator(IUnitOfWork<SuperChatDbContext> uow)
         {
+            _uow = uow;
             //
             RuleFor(x => x.FromUser)
                 .NotEmpty()
@@ -19,6 +26,24 @@ namespace SuperChat.BL.Validations
             RuleFor(x => x.MessageText)
                 .NotEmpty()
                 .WithMessage("The message cannot be empty");
+
+            RuleFor(x => x.ChatRoomId)
+                .NotEqual(0)
+                .WithMessage("The chat room id cannot be empty");
+
+            RuleFor(x => x.ChatRoomId)
+                .MustAsync(Exists)
+                .WithMessage("This chat room does not exist");
+        }
+
+        private async Task<bool> Exists(int chatRoomId, CancellationToken arg2)
+        {
+            var chatRoom = await _uow.GetRepository<ChatRoom>()
+                            .GetById(chatRoomId);
+
+            var chatRoomExists = chatRoom != null;
+
+            return chatRoomExists;
         }
     }
 }
